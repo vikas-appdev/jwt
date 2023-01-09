aba8576d67a29e4e9e3b5648f270e8926f46a4a5
Vikas Yadav <v.ray96610@gmail.com>
2023-01-09 23:56:05 +0530

    UserDetails Service

---
+++ src/main/java/com/gradlic/jwt/service/UserService.java
@@ -0,0 +1,5 @@
+package com.gradlic.jwt.service;
+
+
+public interface UserService {
+}
---
+++ src/main/java/com/gradlic/jwt/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.gradlic.jwt.service.impl;
+
+import com.gradlic.jwt.domain.User;
+import com.gradlic.jwt.domain.UserPrincipal;
+import com.gradlic.jwt.repository.UserRepository;
+import com.gradlic.jwt.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+@Service
+@Transactional
+@Qualifier("UserDetailsService")
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if (user == null){
+            LOGGER.error("User not found with given username: "+username);
+            throw new UsernameNotFoundException("User not found with given username: "+username);
+        }else {
+            user.setLastLoginDateDisplay(user.getLastLoginDate());
+            user.setLastLoginDate(new Date());
+            userRepository.save(user);
+            UserPrincipal userPrincipal = new UserPrincipal(user);
+            LOGGER.info("Returning found user by username: "+username);
+            return userPrincipal;
+        }
+    }
+}
